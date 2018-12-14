15307dc2391f2ae0dc6eb0b2f71bd907d7e14977
bekeplar <bekeplar@gmail.com>
2018-12-14 12:32:52 -0800

    Add changes to capitalize.py file

--- capitalize.py
+++ capitalize.py
@@ -1,2 +1,11 @@
-words = input('sentence: ')
-print(words.upper())
+#words = input('sentence: ')
+# printing a given sentence in capital letters.
+#print(words.upper())
+def capitalize(sen):
+    """Function to capitalize a given function."""
+    return sen.upper()
+
+if __name__ == '__main__':
+    sen = input('Please type a sequence of characters \n')
+    """Asking for user input"""
+    print(capitalize(sen))
